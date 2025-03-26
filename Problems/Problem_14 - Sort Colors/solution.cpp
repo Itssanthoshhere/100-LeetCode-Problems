7fc37cbdce4c5d2dd115ede88c87f629f73958e0
Itssanthoshhere <santhosh02vs@gmail.com>
2025-03-26 19:54:29 +0530

    Problem - 14

--- Problems/Problem_14 - Sort Colors/solution.cpp
+++ Problems/Problem_14 - Sort Colors/solution.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int low = 0, mid = 0, high = n - 1;
-        // Dutch National Flag Algorithm 🎨
-        while(mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else { // nums[mid] == 2
-                swap(nums[mid], nums[high]);
-                high--;
+    int majorityElement(vector<int>& nums) {
+        int count = 1;
+        int result = nums[0];
+        // Boyer-Moore Voting Algorithm 🗳️
+        for (int i = 1; i < nums.size(); i++) {
+            if (count == 0) {
+                result = nums[i];
+            }
+            if (result == nums[i]) {
+                count++;
+            } else {
+                count--;
             }
         }
+        return result;
     }
 };
 
 int main() {
     Solution sol;
-    vector<int> nums = {2,0,2,1,1,0};  // Example input
-    sol.sortColors(nums);
-    cout << "Sorted Colors: ";
-    for (int num : nums) {
-        cout << num << " ";
-    }
-    cout << endl;
+    vector<int> nums = {2,2,1,1,1,2,2};  // Example input
+    cout << "Majority Element: " << sol.majorityElement(nums) << endl;
     return 0;
 }
